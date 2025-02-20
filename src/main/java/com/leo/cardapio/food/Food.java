77c3662da1cc6bb1a98d36ae0af172abb1188eb1
Leonardo Rocha <leonardorocha790@gmail.com>
2025-02-20 16:31:12 -0300

    feature: finished backend project

---
+++ src/main/java/com/leo/cardapio/food/Food.java
@@ -0,0 +1,28 @@
+package com.leo.cardapio.food;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "foods")
+@Table(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Double price;
+
+    public Food(FoodRequestDTO data){
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+    }
+}
---
+++ src/main/java/com/leo/cardapio/food/FoodRequestDTO.java
@@ -0,0 +1,4 @@
+package com.leo.cardapio.food;
+
+public record FoodRequestDTO(String title, String image, Double price) {
+}
