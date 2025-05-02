7d1b1593c7db21e5e7189c0723a7c27be939fe61
Leonardo Rocha <leonardorocha790@gmail.com>
2025-05-01 21:27:57 -0300

    fix: moved from the right package

--- src/main/java/com/leo/cardapio/repositories/FoodRepository.java
+++ src/main/java/com/leo/cardapio/repositories/FoodRepository.java
@@ -1,5 +1,6 @@
-package com.leo.cardapio.food;
+package com.leo.cardapio.repositories;
 
+import com.leo.cardapio.food.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
