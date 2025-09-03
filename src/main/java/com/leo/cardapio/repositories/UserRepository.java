e853c4b1cf31d0ca5e050c7f7ee841b5f4a5004c
Leonardo Rocha <leonardorocha790@gmail.com>
2025-09-03 11:50:23 -0300

    feat: adding findUserByEmail method to the repository

--- src/main/java/com/leo/cardapio/repositories/UserRepository.java
+++ src/main/java/com/leo/cardapio/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.leo.cardapio.repositories;
 import com.leo.cardapio.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByPhone(String phone);
     boolean existsByCpf(String cpf);
+    Optional<User> findUserByEmail(String email);
 }
