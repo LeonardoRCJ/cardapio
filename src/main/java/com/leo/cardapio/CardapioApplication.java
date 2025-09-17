293986f48ee43f6e4759e6882b5b9128b03ebae6
Leonardo Rocha Cavalcante <leonardorocha790@gmail.com>
2025-09-17 16:56:08 -0300

    adding mercado pago access token

--- src/main/java/com/leo/cardapio/CardapioApplication.java
+++ src/main/java/com/leo/cardapio/CardapioApplication.java
@@ -1,13 +1,22 @@
 package com.leo.cardapio;
 
+import com.mercadopago.MercadoPagoConfig;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardapioApplication {
+	@Value("${mercadopago.access_token}")
+	private String mpAccessToken;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardapioApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		MercadoPagoConfig.setAccessToken(mpAccessToken);
+	}
 }
