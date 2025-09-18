6223c8a051bc58a8b7b67d94f7831a95fc093038
Leonardo Rocha <leonardorocha790@gmail.com>
2025-09-18 10:22:54 -0300

    enabling async in main application

--- src/main/java/com/leo/cardapio/CardapioApplication.java
+++ src/main/java/com/leo/cardapio/CardapioApplication.java
@@ -5,8 +5,11 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 
 @SpringBootApplication
+@EnableAsync
 public class CardapioApplication {
 	@Value("${mercadopago.access_token}")
 	private String mpAccessToken;
